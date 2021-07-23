8e8afe5cf4803aba80604c258b0e2bc8b0eaec79
Mohammed Mudassir Ali <72135395+mdmudassirali09@users.noreply.github.com>
2021-07-23 12:30:39 +0530

    Add files via upload

---
+++ Road Repair/Solution.py
@@ -0,0 +1,22 @@
+from collections import Counter
+def stringAnagram(dictionary, query):
+    # Write your code here
+    d = ["".join(sorted(word)) for word in dictionary]
+    q = ["".join(sorted(word)) for word in query]
+    count = Counter(d)
+    for w in q:
+        if w in count.keys():
+            print(count[w])
+        else:
+            print(0)
+            
+if __name__ == "__main__":
+    dictionary_count=int(input())
+    dictionary=[]
+    for i in range(dictionary_count):
+        dictionary.append(input())
+    query_count=int(input())
+    query=[]
+    for i in range(query_count):
+        query.append(input())
+    stringAnagram(dictionary, query)
